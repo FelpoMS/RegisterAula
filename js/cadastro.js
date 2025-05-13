3395297a5e53138e1b684156c90d616ed83c942d
FelpoMS <felipemacielsierote@gmail.com>
2025-05-13 20:34:19 -0300

    d2
    segunda aula

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,14 +1,38 @@
 const botao=document.getElementById('btnCadastrar');
+let usuarios = [];
 
-botao.addEventListener('click',function(){
+// adiciona um evento ao botão , com o click ele vai executar uma função.
+botao.addEventListener('click',function(){ 
 
     const usuario= {
         login: document.getElementById('login').value,
         senha: document.getElementById('senha').value
     }
-    let usuarios = [];
     usuarios.push(usuario);
     console.log(usuarios); 
     let listaUsuarios= JSON.stringify(usuarios);
     localStorage.setItem("usuarios",listaUsuarios);
 });
+
+function listar(){
+    const listaUsuarioCad = JSON.parse(localStorage.getItem("usuarios")) || [];
+    console.log(listaUsuarioCad);
+    const tabelaListaUsuarios = document.getElementById('listaUsuarios');
+    tabelaListaUsuarios.innerHTML='';
+
+
+    listaUsuarioCad.forEach(usuario,index =>{
+        const linha = document.createElement('tr');
+        linha.innerHTML = 
+        `
+        <td>${usuarion.login}</td>
+        <td>${usuarion.senha}</td>
+        <td>
+            <button onclick="excluirUsuario(${index})>Remover</button>
+        </td>
+        `;
+        tabelaListaUsuarios.appendChild(linha);
+    });
+}
+
+listar();
