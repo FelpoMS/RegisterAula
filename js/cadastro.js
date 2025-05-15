a4f1de183ae42cb97aa08856f6813719ebc302d2
FelpoMS <felipemacielsierote@gmail.com>
2025-05-15 20:34:56 -0300

    v2

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,38 +1,47 @@
-const botao=document.getElementById('btnCadastrar');
-let usuarios = [];
+// cadastro 
+const botao = document.getElementById('btnCadastrar');
 
-// adiciona um evento ao botão , com o click ele vai executar uma função.
-botao.addEventListener('click',function(){ 
 
-    const usuario= {
-        login: document.getElementById('login').value,
-        senha: document.getElementById('senha').value
+// cadastrar
+botao.addEventListener('click',
+    function () {
+        let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+        const usuario = {
+            login: document.getElementById('login').value,
+            senha: document.getElementById('senha').value
+        }
+        usuarios.push(usuario);
+        let listaUsuarios = JSON.stringify(usuarios);
+        localStorage.setItem("usuarios", listaUsuarios);
+        listar();
     }
-    usuarios.push(usuario);
-    console.log(usuarios); 
-    let listaUsuarios= JSON.stringify(usuarios);
-    localStorage.setItem("usuarios",listaUsuarios);
-});
-
+);
 function listar(){
     const listaUsuarioCad = JSON.parse(localStorage.getItem("usuarios")) || [];
     console.log(listaUsuarioCad);
     const tabelaListaUsuarios = document.getElementById('listaUsuarios');
-    tabelaListaUsuarios.innerHTML='';
+    tabelaListaUsuarios.innerHTML = "";
 
-
-    listaUsuarioCad.forEach(usuario,index =>{
+    listaUsuarioCad.forEach((usuario, index) => {
         const linha = document.createElement('tr');
-        linha.innerHTML = 
-        `
-        <td>${usuarion.login}</td>
-        <td>${usuarion.senha}</td>
-        <td>
-            <button onclick="excluirUsuario(${index})>Remover</button>
-        </td>
+        linha.innerHTML = `
+            <td>${usuario.login}</td>
+            <td>${usuario.senha}</td>
+            <td>
+                <button onclick="excluirUsuario(${index})">Remover</button>
+            </td>
         `;
         tabelaListaUsuarios.appendChild(linha);
     });
 }
-
+function excluirUsuario(index){
+    const listaUsuarioCad  = JSON.parse(localStorage.getItem("usuarios")) || [];
+ 
+ if(confirm("Voce realmente deseja excluir?")){
+    listaUsuarioCad.splice(index,1);
+    listaJson=JSON.stringify(listaUsuarioCad);
+    localStorage.setItem("usuarios",listaJson);
+    listar();
+ }   
+}
 listar();
